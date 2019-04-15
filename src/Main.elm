d4733dde4bbdb20e9d899ed2ef9f9e5a458b0ca9
Max P <maxfarseer@gmail.com>
2019-04-15 13:06:55 +0300

    add (Main) Score 2/3 actions & ui, Add/Edit ui, total points

--- src/Main.elm
+++ src/Main.elm
@@ -110,6 +110,30 @@ edit model id =
     }
 
 
+updatePlayerPoints : Model -> Player -> Int -> Model
+updatePlayerPoints model scorer points =
+    let
+        newPlayers =
+            List.map
+                (\player ->
+                    if player.id == scorer.id then
+                        { player | points = player.points + points }
+
+                    else
+                        player
+                )
+                model.players
+
+        newPlaysRecord =
+            { id = List.length model.plays
+            , playerId = scorer.id
+            , name = scorer.name
+            , points = points
+            }
+    in
+    { model | players = newPlayers, plays = newPlaysRecord :: model.plays }
+
+
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
@@ -122,6 +146,12 @@ update msg model =
 
             else
                 ( save model, Cmd.none )
+
+        Edit player ->
+            ( { model | name = player.name, playerId = Just player.id }, Cmd.none )
+
+        Score player points ->
+            ( updatePlayerPoints model player points, Cmd.none )
 
         Cancel ->
             ( { model | name = "", playerId = Nothing }, Cmd.none )
@@ -145,6 +175,7 @@ view model =
     Html.div [ Attr.class "scoreboard" ]
         [ Html.h1 []
             [ Html.text "Score Keeper" ]
+        , playerSection model
         , playerForm model
         , Html.p
             []
@@ -163,10 +194,73 @@ playerForm model =
             ]
             []
         , Html.button [ Attr.type_ "submit" ]
-            [ Html.text "Add" ]
+            [ Html.text "Save" ]
         , Html.button
             [ Attr.type_ "button"
             , Events.onClick Cancel
             ]
             [ Html.text "Cancel" ]
+        ]
+
+
+playerSection : Model -> Html Msg
+playerSection model =
+    Html.div []
+        [ playerListHeader
+        , playerList model
+        , pointTotal model
+        ]
+
+
+playerListHeader : Html Msg
+playerListHeader =
+    Html.header []
+        [ Html.div [] [ Html.text "Name" ]
+        , Html.div [] [ Html.text "Points" ]
+        ]
+
+
+playerList : Model -> Html Msg
+playerList model =
+    model.players
+        |> List.sortBy .name
+        |> List.map playerEntry
+        |> Html.ul []
+
+
+playerEntry : Player -> Html Msg
+playerEntry player =
+    Html.li []
+        [ Html.i
+            [ Attr.class "edit"
+            , Events.onClick (Edit player)
+            ]
+            [ Html.text "[EDIT]" ]
+        , Html.div []
+            [ Html.text player.name ]
+        , Html.button
+            [ Attr.type_ "button"
+            , Events.onClick (Score player 2)
+            ]
+            [ Html.text "2pt" ]
+        , Html.button
+            [ Attr.type_ "button"
+            , Events.onClick (Score player 3)
+            ]
+            [ Html.text "3pt" ]
+        , Html.div []
+            [ Html.text (String.fromInt player.points) ]
+        ]
+
+
+pointTotal : Model -> Html Msg
+pointTotal model =
+    let
+        totalPoints =
+            List.map .points model.players |> List.sum
+    in
+    Html.div
+        []
+        [ Html.div [] [ Html.text "Total points" ]
+        , Html.div [] [ Html.text <| String.fromInt totalPoints ]
         ]
